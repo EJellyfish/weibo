15ccfcdb9f3ac778c8bc66c31633a8b864a3f27e
ejellyfish <844699335@qq.com>
2018-02-06 16:42:22 +0800

    Finish static pages

---
+++ resources/views/static_pages/home.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.default')
+@section('title', '微博主页')
+@section('content')
+  <div class="jumbotron">
+    <h1>Hello Laravel</h1>
+    <p class="lead">
+      你现在所看到的是 <a href="#">小哥就是闷闷闷油瓶</a> 的微博主页。
+    </p>
+    <p>
+      一切，将从这里开始。
+    </p>
+    <p>
+      <a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">现在注册</a>
+    </p>
+  </div>
+@stop
