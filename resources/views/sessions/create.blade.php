2bae57ce55c59319afca91d41539cdf2e5a55d79
ejellyfish <844699335@qq.com>
2018-02-07 17:53:42 +0800

    Finish login and logout

--- resources/views/sessions/create.blade.php
+++ resources/views/sessions/create.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.default')
 @section('title', '登录')
 @section('content')
-<div class="col-md-offset-2 col-md-8">
+<div class="col-md-offset-3 col-md-6">
 	<div class="panel panel-default">
 		<div class="panel-heading">
 			<h5>登录</h5>
@@ -9,31 +9,43 @@
 		<div class="panel-body">
 			@include('shared._errors')
 
-			<form method="POST" action="{{ route('login') }}">
+			<form method="POST" action="{{ route('login') }}" class="form-horizontal">
 				{{ csrf_field() }}
 
 				<div class="form-group">
-					<label for="email">邮箱：</label>
-					<input type="text" name="email" class="form-control" value="{{ old('email') }}">
+					<label for="email" class="col-md-3 control-label">邮箱：</label>
+                    <div class="col-md-8">
+                        <input type="text" name="email" class="form-control" value="{{ old('email') }}">
+                    </div>
 				</div>
 
 				<div class="form-group">
-					<label for="password">密码：</label>
-					<input type="password" name="password" class="form-control" value="{{ old('password') }}">
+					<label for="password" class="col-md-3 control-label">密码：</label>
+                    <div class="col-md-8">
+                        <input type="password" name="password" class="form-control" value="{{ old('password') }}">
+                    </div>
 				</div>
 
-                <div class="checkbox">
-                    <label><input type="checkbox" name="remenber">记住我</label>
+                <div class="form-group">
+                    <div class="checkbox col-sm-offset-3 col-md-9">
+                        <label><input type="checkbox" name="remenber">记住我</label>
+                    </div>
                 </div>
 
-				<button type="submit" class="btn btn-primary">登录</button>
+                <div class="form-group">
+                    <button type="submit" class="btn btn-primary col-sm-offset-3 col-md-6">登录</button>
+                </div>
+
 			</form>
 
 			<hr>
 
-			<p>还没账号？
-				<a href="{{ route('signup') }}">现在注册！</a>
-			</p>
+            <div class="col-sm-offset-3 col-md-9">
+                <p>还没账号？
+                    <a href="{{ route('signup') }}">现在注册！</a>
+                </p>
+            </div>
+
 		</div>
 	</div>
 </div>
