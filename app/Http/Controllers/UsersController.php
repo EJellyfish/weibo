b8a95aaafd2447639f077069a616fac98ebd6308
ejellyfish <844699335@qq.com>
2018-03-23 16:34:44 +0800

    Add user following

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -130,4 +130,19 @@ class UsersController extends Controller
         return redirect()->route('users.show', [$user]);
     }
 
+    // 用户关注的人列表
+    public function followings(User $user)
+    {
+        $users = $user->followings()->paginate(30);
+        $title = '关注的人';
+        return view('users.show_follow', compact('users', 'title'));
+    }
+
+    // 用户粉丝列表
+    public function followers(User $user)
+    {
+        $users = $user->followers()->paginate(30);
+        $title = '粉丝';
+        return view('users.show_follow', compact('users', 'title'));
+    }
 }
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -6,14 +6,17 @@
     <div class="row">
       <div class="col-md-8">
         <section class="status_form">
-          @include('shared._status_form')
+        @include('shared._status_form')
         </section>
         <h3>微博列表</h3>
         @include('shared._feed')
       </div>
       <aside class="col-md-4">
         <section class="user_info">
-          @include('shared._user_info', ['user' => Auth::user()])
+            @include('shared._user_info', ['user' => Auth::user()])
+        </section>
+        <section class="stats">
+            @include('shared._stats', ['user' => Auth::user()])
         </section>
       </aside>
     </div>
